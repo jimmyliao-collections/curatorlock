9e046f02d39087d3d9b167e50806d99c431dab4a
Jimmy Liao (IBM) <sjliao@tw.ibm.com>
2014-02-14 14:53:55 +0800

    Initial commit

---
+++ app/build/source/buildConfig/debug/im/curator/lock/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package im.curator.lock;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String PACKAGE_NAME = "im.curator.lock";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+}
